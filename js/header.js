a212f9505d618c5f5fc714bd70971159a5e58359
liliia_vintiuk <rudapanda2016@gmail.com>
2024-02-27 17:27:09 +0200

    dont up

--- js/header.js
+++ js/header.js
@@ -4,10 +4,10 @@ function header() {
 
     const HTML = `
         <a href="${href}">Home</a>
-        <a href="${href}single-button-click/index.html">Single button click</a>
-        <a href="${href}counter/index.html">Counter</a>
-        <a href="${href}basketball/index.html">Basketball</a>
-        <a href="${href}virykle/index.html">Virykle</a>
+        <a href="${href}single-button-click/">Single button click</a>
+        <a href="${href}counter/">Counter</a>
+        <a href="${href}basketball/">Basketball</a>
+        <a href="${href}virykle/">Virykle</a>
     `;
     navDOM.innerHTML = HTML;
 }
