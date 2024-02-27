59a4f5b86192bab70b99861fc17cc2c7d7e83bd8
liliia_vintiuk <rudapanda2016@gmail.com>
2024-02-27 15:18:33 +0200

    up

--- js/header.js
+++ js/header.js
@@ -1,13 +1,13 @@
 function header() {
     const navDOM = document.querySelector('.main-nav');
-    const href = location.hostname === 'localhost' ? '/' : './';
+    const href = location.hostname === 'localhost 4810' ? '/' : './';
 
     const HTML = `
         <a href="${href}">Home</a>
-        <a href="single-button-click">Single button click</a>
-        <a href="counter/">Counter</a>
-        <a href="basketball/">Basketball</a>
-        <a href="virykle/">Virykle</a>
+        <a href="${href}single-button-click">Single button click</a>
+        <a href="${href}counter/">Counter</a>
+        <a href="${href}basketball/">Basketball</a>
+        <a href="${href}virykle/">Virykle</a>
     `;
     navDOM.innerHTML = HTML;
 }
