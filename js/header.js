121f5004c9264e2768fef68d912af089ea616778
liliia_vintiuk <rudapanda2016@gmail.com>
2024-02-27 21:06:34 +0200

    siano up

--- js/header.js
+++ js/header.js
@@ -1,17 +1,15 @@
 function header() {
     const navDOM = document.querySelector('.main-nav');
-    const href = location.hostname === "liliiavint.github.io" ? '/' : './';
-    
+    const href = location.hostname === 'localhost' ? '/' : './';
+
     const HTML = `
-      
+        <base href="/48-grupe-dom-events/">
         <a href="${href}">Home</a>
         <a href="${href}single-button-click/">Single button click</a>
         <a href="${href}counter/">Counter</a>
         <a href="${href}basketball/">Basketball</a>
         <a href="${href}virykle/">Virykle</a>
-      
+        </base>
     `;
     navDOM.innerHTML = HTML;
 }
-
-export { header };
