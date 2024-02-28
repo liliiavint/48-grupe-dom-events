a6c8a100275958b47534e8f920597c5319e7f7e9
liliia_vintiuk <rudapanda2016@gmail.com>
2024-02-28 09:13:02 +0200

    up

--- js/header.js
+++ js/header.js
@@ -2,9 +2,7 @@ function header() {
     const navDOM = document.querySelector('.main-nav');
     const localhost = 'http://localhost:4810';
     const github = 'https://liliiavint.github.io/48-grupe-dom-events';
-    
     const href = location.hostname === 'localhost' ? localhost : github;
-
 
     const HTML = `
         <a href="${href}/">Home</a>
@@ -12,6 +10,7 @@ function header() {
         <a href="${href}/counter/">Counter</a>
         <a href="${href}/basketball/">Basketball</a>
         <a href="${href}/virykle/">Virykle</a>
+        <a href="${href}/cart/">Prekiu krepselis</a>
     `;
     navDOM.innerHTML = HTML;
 }
