a1b3990cdccd14a3c719ebf8a4d0aea599953c19
liliia_vintiuk <rudapanda2016@gmail.com>
2024-02-28 09:04:50 +0200

    up

--- js/header.js
+++ js/header.js
@@ -1,15 +1,17 @@
 function header() {
     const navDOM = document.querySelector('.main-nav');
-    const href = location.hostname === "liliiavint.github.io/48-grupe-dom-events" ? '/' : './';
+    const localhost = 'http://localhost:4810';
+    const github = 'https://liliiavint.github.io/48-grupe-dom-events';
     
+    const href = location.hostname === 'localhost' ? localhost : github;
+
+
     const HTML = `
-      
-        <a href="${href}">Home</a>
-        <a href="${href}single-button-click/">Single button click</a>
-        <a href="${href}counter/">Counter</a>
-        <a href="${href}basketball/">Basketball</a>
-        <a href="${href}virykle/">Virykle</a>
-      
+        <a href="${href}/">Home</a>
+        <a href="${href}/single-button-click">Single button click</a>
+        <a href="${href}/counter/">Counter</a>
+        <a href="${href}/basketball/">Basketball</a>
+        <a href="${href}/virykle/">Virykle</a>
     `;
     navDOM.innerHTML = HTML;
 }
