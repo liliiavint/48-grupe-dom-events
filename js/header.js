b25cf93ce1a9cd523ccd3798fb7cc5bda5ff26a5
Lilia <153711747+liliiavint@users.noreply.github.com>
2024-02-28 05:39:26 +0200

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,15 +1,15 @@
 function header() {
     const navDOM = document.querySelector('.main-nav');
-    const href = location.hostname === "lokalhostame" ? '/' : './';
+    const href = location.hostname === "https://liliiavint.github.io/" ? '/' : './';
     
     const HTML = `
-      <base href="/48-grupe-dom-events/>
+     
         <a href="${href}">Home</a>
         <a href="${href}single-button-click/">Single button click</a>
         <a href="${href}counter/">Counter</a>
         <a href="${href}basketball/">Basketball</a>
         <a href="${href}virykle/">Virykle</a>
-</base>
+
 
       
     `;
