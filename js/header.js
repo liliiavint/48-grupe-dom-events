dfc3d4ea1c213bb5628984b682812570beee4872
Lilia <153711747+liliiavint@users.noreply.github.com>
2024-02-28 05:50:34 +0200

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,17 +1,13 @@
 function header() {
     const navDOM = document.querySelector('.main-nav');
-    const href = location.hostname === "https://liliiavint.github.io/" ? '/' : './';
+    const href = location.hostname === "liliiavint.github.io" ? '/' : '/48-grupe-dom-events/';
     
     const HTML = `
-     
         <a href="${href}">Home</a>
         <a href="${href}single-button-click/">Single button click</a>
         <a href="${href}counter/">Counter</a>
         <a href="${href}basketball/">Basketball</a>
         <a href="${href}virykle/">Virykle</a>
-
-
-      
     `;
     navDOM.innerHTML = HTML;
 }
