4f8546b661fd9c74a46a65c1dbbec9dde06a54a2
liliia_vintiuk <rudapanda2016@gmail.com>
2024-03-03 19:54:27 +0200

    clock

--- js/header.js
+++ js/header.js
@@ -1,9 +1,8 @@
 function header() {
     const navDOM = document.querySelector('.main-nav');
     const localhost = 'http://localhost:4810';
-    const github = 'https://liliiavint.github.io/48-grupe-dom-events';
-    const liveserver = 'http://127.0.0.1:5501';
-    const href = location.hostname === 'localhost' ? localhost : (location.hostname === 'liliiavint.github.io' ? github : liveserver);
+    const github = 'https://front-end-by-rimantas.github.io/48-grupe-dom-events';
+    const href = location.hostname === 'localhost' ? localhost : github;
 
     const HTML = `
         <a href="${href}/">Home</a>
@@ -12,6 +11,8 @@ function header() {
         <a href="${href}/basketball/">Basketball</a>
         <a href="${href}/virykle/">Virykle</a>
         <a href="${href}/cart/">Prekiu krepselis</a>
+        <a href="${href}/clock/">Laikrodis</a>
+        <a href="${href}/better-clock/">Geresnis laikrodis</a>
     `;
     navDOM.innerHTML = HTML;
 }
