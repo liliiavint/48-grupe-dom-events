2bbeb312c58be3d0b5ac1ed3ff5f594e8f96477f
liliia_vintiuk <rudapanda2016@gmail.com>
2024-03-05 21:13:51 +0200

    update user interface

--- js/user-interface.js
+++ js/user-interface.js
@@ -2,13 +2,14 @@ import { header } from "./header.js";
 header();
 
 function showPage(page){
-    document.querySelectorAll('div .y').forEach(div => {
+    document.querySelectorAll('div').forEach(div => {
         div.style.display = 'none';
     })
     document.querySelector(`#${page}`).style.display = 'block';
 }
-document.addEventListener('DOMContentLoaded', function (){
-    document.querySelectorAll('button .x').forEach(button =>{
+
+document.addEventListener('click', function (){
+    document.querySelectorAll('button').forEach(button =>{
         button.onclick = function () {
             showPage(this.dataset.page);
                   
